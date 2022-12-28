1352dbfe1542751fdea8a33316087da5eacc8efb
Nathan Woody <nathan@nathanwoody.com>
2022-12-28 05:39:49 +0000

    Commit to latest

--- src/frk_incrementalencoder.py
+++ src/frk_incrementalencoder.py
@@ -1,26 +1,32 @@
-from framework import Driver
+import asyncio
 import rotaryio
 
-class IncrementalEncoder(Driver):
-    _defaults = {'divisor': 2,
-                 'reverse': False,
-                 'position': 0,
-                 'event': False,
-                 'increased': False,
-                 'decreased': False,
-                 'on_event': [],
-                 'on_incresed': [],
-                 'on_decreased': []}
-
+class IncrementalEncoder:
+    sleep = 0.01
+    divisor = 4
+    position = 0
+    
+    reverse = False
+    
+    event = False
+    increased = False
+    decreased = False
+    on_event = []
+    on_increased = []
+    on_decreased = []
+    
     def _init_device(self):
         self._device = rotaryio.IncrementalEncoder(*self._pins, divisor=self._divisor)
-        self.__position = self._position = self._device.position
-
-    def _loop(self):
-        self._position = self._device.position * {True: -1, False: 1}[self._reverse]
-        if self.__position < self._position:
-            self._handle_event('increased', self._position)
-        if self.__position > self._position:
-            self._handle_event('decreased', self._position)
-        if self.__position != self._position:
-            self._handle_event('event', self._position)
+    
+    async def _run(self):
+        p = self._position
+        while True:
+            self._position = self._device.position * {True: -1, False: 1}[self._reverse]
+            if self._position != p:
+                if self._position > p:
+                    self._handle_event("increased", self._position)
+                if self._position < p:
+                    self._handle_event("decreased", self._position)
+                self._handle_event("event", self._position)
+                p = self._position
+            await asyncio.sleep(self._sleep)
